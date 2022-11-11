a1ece68ff6653acfccae41d6a7a1ffcf7aad5433
Sayantan40 <sayantanmitra.kol28@gmail.com>
2022-11-11 22:00:46 +0530

    constants updated

--- src/BigMartSales/constants/__init__.py
+++ src/BigMartSales/constants/__init__.py
@@ -9,7 +9,7 @@ def get_current_time_stamp():
 
 
 ROOT_DIR = os.getcwd()  #to get current working directory
-CONFIG_DIR = "configs"
+CONFIG_DIR = "config"
 CONFIG_FILE_NAME = "config.yaml"
 CONFIG_FILE_PATH = os.path.join(ROOT_DIR,CONFIG_DIR,CONFIG_FILE_NAME)
 
